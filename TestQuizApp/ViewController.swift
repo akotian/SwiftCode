e8e8100ba52e0c29a67bdec6d4a1bd3aed822e98
Ashley R Kotian <akotian@C02P5376G3QK.lan>
2015-12-09 11:57:30 -0800

    Initial Commit

---
+++ TestQuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestQuizApp
+//
+//  Created by Ashley R Kotian on 12/9/15.
+//  Copyright (c) 2015 Livingly Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
